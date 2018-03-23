207631663b76fef0fcd5924e5dfe4d0bd585c346
HFU Akademie <HFU Akademie@LAPTOP-K1GJGVVI.hfu-open.fu.hs-furtwangen.de>
2018-03-23 14:15:42 +0100

    Aufgabe0 hochgeladen

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -1,8 +1,8 @@
 function Hallo() {
-    var name = prompt("Wie heißt du?", "");
+    var name = prompt("Wie ist dein Name?", "");
     if (name != null) {
         document.getElementById("Begrüßung").innerHTML =
-            "Hallo " + name + "! Willkommen auf meiner Seite.Viel Spa�";
+            "Hallo " + name + "! Willkommen auf meiner Seite.Viel Spaß";
     }
 }
 //# sourceMappingURL=Aufgabe0.js.map
