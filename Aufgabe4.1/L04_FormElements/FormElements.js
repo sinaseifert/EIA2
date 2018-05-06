2f3f3ba0eae2edd9885c74b7f7ea0f50572f2703
sinaseifert <37697857+sinaseifert@users.noreply.github.com>
2018-05-06 17:55:49 +0200

    Add files via upload

---
+++ Aufgabe4.1/L04_FormElements/FormElements.js
@@ -0,0 +1,36 @@
+var L04_FormElements;
+(function (L04_FormElements) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Init");
+        var fieldsets = document.getElementsByTagName("fieldset");
+        for (var i = 0; i < fieldsets.length; i++) {
+            var fieldset = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+        }
+    }
+    function handleChange(_event) {
+        //console.log(_event);
+        //*/
+        var target = _event.target;
+        console.log("Changed " + target.name + " to " + target.value);
+        //*/
+        //*/ note: this == _event.currentTarget in an event-handler
+        if (this.id == "checkbox")
+            console.log("Changed " + target.name + " to " + target.checked);
+        //*/
+        //*/
+        if (target.name == "Slider") {
+            var progress = document.getElementsByTagName("progress")[0];
+            progress.value = parseFloat(target.value);
+        }
+        //*/
+        //*/
+        if (target.name == "Stepper") {
+            var meter = document.getElementsByTagName("meter")[0];
+            meter.value = parseFloat(target.value);
+        }
+        //*/
+    }
+})(L04_FormElements || (L04_FormElements = {}));
+//# sourceMappingURL=FormElements.js.map
