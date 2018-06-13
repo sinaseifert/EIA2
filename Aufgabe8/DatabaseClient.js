be90e8418d5360b8866a4faa68a05b562016ce5a
sinaseifert <37697857+sinaseifert@users.noreply.github.com>
2018-06-13 11:23:54 +0200

    korrigiert

--- Aufgabe8/DatabaseClient.js
+++ Aufgabe8/DatabaseClient.js
@@ -15,21 +15,21 @@ var DatabaseClient;
     function insert(_event) {
         let genderButton = document.getElementById("male");
         let matrikel = inputs[2].value;
-        let courseOfStudies = document.getElementById("options");
+        let courseOfStudies = inputs[6].value;
         let student;
-        let tempCourseOfStudies = document.getElementById("options");
+        //        let tempCourseOfStudies: HTMLInputElement = <HTMLInputElement>document.getElementById("options");
         student = {
             name: inputs[0].value,
             firstname: inputs[1].value,
             matrikel: parseInt(matrikel),
             age: parseInt(inputs[3].value),
             gender: genderButton.checked,
-            courseOfStudies: tempCourseOfStudies.value
+            courseOfStudies: inputs[6].value
         };
         let stringifyJSON = JSON.stringify(student);
         console.log(stringifyJSON);
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", address + "?command=insert=" + stringifyJSON, true);
+        xhr.open("GET", address + "?command=insert&data=" + stringifyJSON, true);
         xhr.addEventListener("readystatechange", handleInsertResponse);
         xhr.send();
         //        query += "&name=" + inputs[0].value;
@@ -41,16 +41,30 @@ var DatabaseClient;
         //        console.log(query);
         //        sendRequest(student, handleInsertResponse);
     }
+    function handleInsertResponse(_event) {
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
     function refresh(_event) {
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", address + "?command=refresh=", true);
+        xhr.open("GET", address + "?command=refresh", true);
         xhr.addEventListener("readystatechange", handleRefreshResponse);
         xhr.send();
     }
+    function handleRefreshResponse(_event) {
+        let output = document.getElementsByTagName("textarea")[0];
+        output.value = "";
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            output.value += xhr.response;
+        }
+    }
     function find(_event) {
-        let matrikel = inputs[3].value;
+        let matrikel = inputs[2].value;
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", address + "?command=find=" + matrikel, true);
+        xhr.open("GET", address + "?command=find" + matrikel, true);
         xhr.addEventListener("readystatechange", handleFindResponse);
         xhr.send();
     }
@@ -61,20 +75,6 @@ var DatabaseClient;
     //        xhr.addEventListener("readystatechange", handleInsertResponse);
     //        xhr.send();
     //    }
-    function handleInsertResponse(_event) {
-        let xhr = _event.target;
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            alert(xhr.response);
-        }
-    }
-    function handleRefreshResponse(_event) {
-        let output = document.getElementsByTagName("textarea")[0];
-        output.value = "";
-        let xhr = _event.target;
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            output.value += xhr.response;
-        }
-    }
     function handleFindResponse(_event) {
         let output = document.getElementsByTagName("textarea")[1];
         output.value = "";
--- Aufgabe8/DatabaseClient.ts
+++ Aufgabe8/DatabaseClient.ts
@@ -3,6 +3,7 @@ namespace DatabaseClient {
     let address: string = "https://eia2node1.herokuapp.com/";
 
     let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+
     function init(_event: Event): void {
         console.log("Init");
         let insertButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("insert");
@@ -17,21 +18,21 @@ namespace DatabaseClient {
     function insert(_event: Event): void {
         let genderButton: HTMLInputElement = <HTMLInputElement>document.getElementById("male");
         let matrikel: string = inputs[2].value;
-        let courseOfStudies: HTMLSelectElement = <HTMLSelectElement>document.getElementById("options");
+        let courseOfStudies = inputs[6].value;
         let student: StudentData;
-        let tempCourseOfStudies: HTMLInputElement = <HTMLInputElement>document.getElementById("options");
+        //        let tempCourseOfStudies: HTMLInputElement = <HTMLInputElement>document.getElementById("options");
         student = {
             name: inputs[0].value,
             firstname: inputs[1].value,
             matrikel: parseInt(matrikel),
             age: parseInt(inputs[3].value),
             gender: genderButton.checked,
-            courseOfStudies: tempCourseOfStudies.value
+            courseOfStudies: inputs[6].value
         };
         let stringifyJSON: string = JSON.stringify(student);
         console.log(stringifyJSON);
         let xhr: XMLHttpRequest = new XMLHttpRequest();
-        xhr.open ("GET", address + "?command=insert=" + stringifyJSON, true);
+        xhr.open("GET", address + "?command=insert&data=" + stringifyJSON, true);
         xhr.addEventListener("readystatechange", handleInsertResponse);
         xhr.send();
 
@@ -42,38 +43,22 @@ namespace DatabaseClient {
         //        query += "&gender=" + genderButton.checked;
         //        query += "&courseOfStudies=" + inputs[6].value;
         //        console.log(query);
-//        sendRequest(student, handleInsertResponse);
+        //        sendRequest(student, handleInsertResponse);
 
     }
-
-    function refresh(_event: Event): void {
-        let xhr: XMLHttpRequest = new XMLHttpRequest();
-        xhr.open("GET", address + "?command=refresh=", true);
-        xhr.addEventListener("readystatechange", handleRefreshResponse);
-        xhr.send();
-    }
-
-    function find(_event: Event): void {
-        let matrikel: string = inputs[3].value;
-        let xhr: XMLHttpRequest = new XMLHttpRequest();
-        xhr.open("GET", address + "?command=find=" + matrikel, true);
-        xhr.addEventListener("readystatechange", handleFindResponse);
-        xhr.send();
-    }
-
-//    function sendRequest(_query: string, _callback: EventListener): void {
-//        let xhr: XMLHttpRequest = new XMLHttpRequest();
-//        xhr.open("GET", address + "?command=insert", true); //https://eia2node1.herokuapp.com/?" + _query, true);
-//        //xhr.open("GET", "https://eia2-w17-databasetest.herokuapp.com/?" + _query, true);
-//        xhr.addEventListener("readystatechange", handleInsertResponse);
-//        xhr.send();
-//    }
 
     function handleInsertResponse(_event: ProgressEvent): void {
         let xhr: XMLHttpRequest = (<XMLHttpRequest>_event.target);
         if (xhr.readyState == XMLHttpRequest.DONE) {
             alert(xhr.response);
         }
+    }
+
+    function refresh(_event: Event): void {
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=refresh", true);
+        xhr.addEventListener("readystatechange", handleRefreshResponse);
+        xhr.send();
     }
 
     function handleRefreshResponse(_event: ProgressEvent): void {
@@ -84,6 +69,22 @@ namespace DatabaseClient {
             output.value += xhr.response;
         }
     }
+
+    function find(_event: Event): void {
+        let matrikel: string = inputs[2].value;
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=find" + matrikel, true);
+        xhr.addEventListener("readystatechange", handleFindResponse);
+        xhr.send();
+    }
+
+    //    function sendRequest(_query: string, _callback: EventListener): void {
+    //        let xhr: XMLHttpRequest = new XMLHttpRequest();
+    //        xhr.open("GET", address + "?command=insert", true); //https://eia2node1.herokuapp.com/?" + _query, true);
+    //        //xhr.open("GET", "https://eia2-w17-databasetest.herokuapp.com/?" + _query, true);
+    //        xhr.addEventListener("readystatechange", handleInsertResponse);
+    //        xhr.send();
+    //    }
 
     function handleFindResponse(_event: ProgressEvent): void {
         let output: HTMLTextAreaElement = document.getElementsByTagName("textarea")[1];
